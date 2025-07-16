7c580af0d93dd2c87a592fe3c597931e1a06ddd4
ehsanmirahamdi <ehsanmirahmadi2007@gmail.com>
2025-07-16 12:55:32 +0330

    fix

--- react-frontend/src/pages/Login.jsx
+++ react-frontend/src/pages/Login.jsx
@@ -33,7 +33,7 @@ export default function Login() {
                             <div className="row g-0">
                                 <div className="col-md-6 col-lg-5 d-none d-md-block">
                                     <img
-                                        src=""
+                                        src="../assets/img/tablo.jpg"
                                         alt="login form" className="img-fluid" style="border-radius: 1rem 0 0 1rem;"/>
                                 </div>
                                 <div className="col-md-6 col-lg-7 d-flex align-items-center">
