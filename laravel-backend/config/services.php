438a2544c26c74346a0ea8a940a55af889fff3a4
ehsanmirahamdi <ehsanmirahmadi2007@gmail.com>
2025-07-21 10:40:05 +0330

    <<<REST ALL PROJECT>>>

--- laravel-backend/config/services.php
+++ laravel-backend/config/services.php
@@ -34,9 +34,5 @@ return [
             'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
         ],
     ],
-    'openrouter' => [
-        'key' => env('OPENROUTER_API_KEY'),
-        'model' => env('OPENROUTER_MODEL'),
-    ],
 
 ];
